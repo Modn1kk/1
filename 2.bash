d90608029e9741c23c5ba75cd29a19c57c493291
Mikhail <83498224+Modn1kk@users.noreply.github.com>
2024-05-08 23:06:16 +0300

    Update 2.bash
    1123123123123

--- 2.bash
+++ 2.bash
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/Modn1kk/1/main/TLS-PRO.js
 wget https://raw.githubusercontent.com/Modn1kk/1/main/ua.txt
 wget https://raw.githubusercontent.com/Modn1kk/1/main/proxy.txt
 apt install screen -y
+apt install psmisc -y
