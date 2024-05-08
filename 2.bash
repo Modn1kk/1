a2dce3fdeb11ff14c221ac68c03bdb4282d06944
Mikhail <83498224+Modn1kk@users.noreply.github.com>
2024-05-08 23:03:47 +0300

    Add files via upload

---
+++ 2.bash
@@ -0,0 +1,7 @@
+apt update -y
+apt install nodejs -y
+apt install npm -y
+wget https://raw.githubusercontent.com/Modn1kk/1/main/TLS-PRO.js
+wget https://raw.githubusercontent.com/Modn1kk/1/main/ua.txt
+wget https://raw.githubusercontent.com/Modn1kk/1/main/proxy.txt
+apt install screen -y
